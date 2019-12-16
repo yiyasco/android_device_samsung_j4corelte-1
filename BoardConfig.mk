1cdc258f4444c1fb3fd514fe31586fd648f75597
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2019-12-16 23:46:15 +0100

    j4corelte: initial device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,93 @@
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/j4corelte
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES :=true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno308
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := RG13A002KU
+TARGET_NO_BOOTLOADER := true
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Kernel
+
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS :=  --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --second_offset 0x00f00000 --tags_offset 0x01e00000
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_KERNEL_CONFIG := j4corelte_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8917
+BOARD_KERNEL_IMAGE_NAME := zImage
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2453520000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12348030976 # 25765059584 - 16384
+BOARD_VENDORIMAGE_PARTITION_SIZE := 106954752
+BOARD_CACHEIMAGE_PARTITION_SIZE :=295698432
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_INCLUDE_CRYPTO := true
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_EXCLUDE_SUPERSU := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_IGNORE_MISC_WIPE_DATA := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_HAS_DOWNLOAD_MODE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_CRYPTO_USE_SYSTEM_VOLD := true
+TW_INCLUDE_NTFS_3G := true
+TW_DEVICE_VERSION := R10.0
+TW_USE_TOOLBOX := true
+
+# OrangeFox Configuration
+OF_DONT_PATCH_ON_FRESH_INSTALLATION := 1
+OF_DISABLE_MIUI_SPECIFIC_FEATURES := 1
+OF_FLASHLIGHT_ENABLE= 1
+
